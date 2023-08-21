6dd828d8b5bf65b025a091da1b837790506e18a6
Ryker Swensen <83606669+RykerSwensen@users.noreply.github.com>
2023-08-21 15:46:51 -0600

    Push day 1 work.

--- README.md
+++ README.md
@@ -2,5 +2,30 @@
 
 # How to use the application
 
-# Where I am 
+# Stack Used
+ReactJS
+Firebase
+TailwindCSS
 
+# What is Completed?
+1. Created a GitHub Repo. 
+2. Invited Nathan Birch as a collaborator. 
+3. Created a Firebase database. 
+4. Added birchn@byui.edu as owner of the DB. 
+5. Created a ReactJS, Firebase, TailwindCSS application. 
+6. Able to add students to firebase through web application. 
+7. Prevent default added when submitting a student name. 
+8. Input empties back to empty after the name is submitted. 
+9. Able to add if task was completed (check box). Submitted to firebase.
+10. Class count appears at bottom of class card. Unless there is 0 students, then no count or text appears.
+11. Some basic tailwind styling to try to match the theme of Nathan Birch Courses.
+12. ENV file created to hide sensitive data (firebase.)
+
+
+# What I need to Do
+Create tables for each class. 
+Login Page. 
+Base 64 images. 
+Student Point System. 
+Point Bar. 
+Host Website.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import {AiOutlinePlus} from 'react-icons/ai'
+import Student from './Student';
+import { db } from './firebase';
+import { query, collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc} from "firebase/firestore";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#800080]`,
+  container: ` bg-slate-100 max-w-[500px] w-full m-auto rounded-lg shadow-lg p-4`,
+  heading: `text-3xl font-bold text-center text-black p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl rounded-lg `,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100 rounded-lg sm:mt-0`,
+  count: `text-center p-2s`,
+}
 
 function App() {
+  const [students, setStudents] = useState([]);
+  const [input, setInput] = useState('');
+
+  // Create student
+  const createStudent = async (e) => {
+    e.preventDefault(e);
+    if (input === '') {
+      alert('Please enter a Student Name');
+      return;
+    }
+    await addDoc(collection(db, 'students'), {
+      text: input,
+      completed: false,
+    });
+    setInput('');
+  };
+
+  // Read student from firebase
+  useEffect(() => {
+    const q = query(collection(db, 'students'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let studentsArr = [];
+      querySnapshot.forEach((doc) => {
+        studentsArr.push({ ...doc.data(), id: doc.id });
+      });
+      setStudents(studentsArr);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  // Update student in firebase
+  const toggleComplete = async (student) => {
+    await updateDoc(doc(db, 'students', student.id), {
+      completed: !student.completed,
+    });
+  };
+
+  // Delete student
+  const deleteStudent = async (id) => {
+    await deleteDoc(doc(db, 'students', id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Reward App</h3>
+        <form onSubmit={createStudent} className={style.form}>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className={style.input}
+            type='text'
+            placeholder='Add Student'
+          />
+          <button className={style.button}>
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul>
+          {students.map((student, index) => (
+            <Student
+              key={index}
+              student={student}
+              toggleComplete={toggleComplete}
+              deleteStudent={deleteStudent}
+            />
+          ))}
+        </ul>
+        {students.length < 1 ? null : (
+          <p className={style.count}>{`There are ${students.length} students in this class.`}</p>
+        )}
+      </div>
     </div>
   );
 }
